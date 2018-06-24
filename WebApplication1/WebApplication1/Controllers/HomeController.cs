1349d664c78e2fbff71fbff8b2b918006e0a5b1c
k2moz <aleksandersazhin@gmail.com>
2018-06-24 16:28:13 +0300

    First method for use DB

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Entityes;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private EFDBContext _context;
+        public HomeController(EFDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             HelloModel _model = new HelloModel() { HelloMessage = "Hey Aleksander!" };
-            return View(_model);
+            List<Directory> _dirs = _context.Directory.Include(x=>x.Materials).ToList();
+            return View(_dirs);
         }
 
         public IActionResult About()
